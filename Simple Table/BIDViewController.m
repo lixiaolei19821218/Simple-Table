7b8b9adc7c0be60b178d0b6ea264b90fa859cd53
lixiaolei19821218 <735534185@qq.com>
2013-04-17 21:43:29 +0800

    Done

--- Simple Table/BIDViewController.m
+++ Simple Table/BIDViewController.m
@@ -30,4 +30,66 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.dwarves count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *SimpleTableIdentifier = @"SimpleTableIdentifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SimpleTableIdentifier];
+    if (cell == nil)
+    {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:SimpleTableIdentifier];
+    }
+    cell.imageView.image = [UIImage imageNamed:@"star.png"];
+    cell.imageView.highlightedImage = [UIImage imageNamed:@"star2.png"];
+    cell.textLabel.text = self.dwarves[indexPath.row];
+    cell.textLabel.font = [UIFont boldSystemFontOfSize:50];
+    if (indexPath.row < 7)
+    {
+        cell.detailTextLabel.text = @"Mr. Disney";
+    }
+    else
+    {
+       cell.detailTextLabel.text = @"Mr. Tolkien";
+    }
+    return cell;
+}
+
+#pragma mark -
+#pragma mark Table Delegate Methods
+
+- (NSInteger)tableView:(UITableView *)tableView indentationLevelForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return indexPath.row;
+}
+
+-(NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0)
+    {
+        return nil;
+    }
+    else
+    {
+        return indexPath;
+    }
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *rowValue = self.dwarves[indexPath.row];
+    NSString *message = [[NSString alloc] initWithFormat:@"You selected %@", rowValue];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Row Selected" message:message delegate:nil cancelButtonTitle:@"Yes I Did" otherButtonTitles:nil];
+    [alert show];
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 70;
+}
 @end
